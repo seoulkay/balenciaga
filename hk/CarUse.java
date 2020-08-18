63225a3982051f1c4fcec7e23cd48bff783bbca0
gracehkpark <gracehkpark@gmail.com>
2020-08-19 08:53:39 +0900

    Update CarUse.java

--- hk/CarUse.java
+++ hk/CarUse.java
@@ -9,6 +9,7 @@ public class CarUse {
 		car1.color="블랙";
 		car1.speed=60;
 		car1.oil=20;
+		car1.image = "https://search.pstatic.net/common?type=f&size=128x76&quality=75&direct=true&src=https%3A%2F%2Fimgauto-phinf.pstatic.net%2F20200526_92%2Fauto_1590456200502mTVlf_PNG%2F20200526102317_a3HegZN5.png";
 		
 		Car car2 = new Car();
 		car2.model="5.0SC";
@@ -16,6 +17,7 @@ public class CarUse {
 		car2.color="그레이";
 		car2.speed=50;
 		car2.oil=40;
+		car2.image = "https://search.pstatic.net/common?type=ofullfill&size=202x152&quality=95&direct=true&src=https%3A%2F%2Fimgauto-phinf.pstatic.net%2F20200508_58%2Fauto_1588901890960anoJM_JPEG%2F20200508103805_ZnrHaL0y.jpg";
 		
 		car1.CarInfo();
 		car1.AccelCar(20);
