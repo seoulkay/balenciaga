33d0e191989b91ad4cfa4dd9cdace230dd2b5dd5
Kay Lee <seoulkay@gmail.com>
2020-08-19 13:23:20 +0900

    Create day7_Reply.java

---
+++ ih/day7_Reply.java
@@ -0,0 +1,14 @@
+package pack3;
+
+public class Reply {
+	String id;
+	String content;
+	String date;
+	
+	//contructor 생성자
+	public Reply(String id, String content, String date){
+		this.id = id;
+		this.content = content;
+		this.date = date;
+	}
+}
