05656576e267aa751d1d70b871fcfedf7abab906
gracehkpark <gracehkpark@gmail.com>
2020-08-20 09:51:26 +0900

    Create Point.java

---
+++ hk/Point.java
@@ -0,0 +1,14 @@
+package pack1;
+
+public class Point {
+	int total;
+	String starPoint;
+	
+	//생성자
+	public Point(int total, String starPoint) {
+		this.total = total;
+		this.starPoint = starPoint;
+		
+	}
+
+}
