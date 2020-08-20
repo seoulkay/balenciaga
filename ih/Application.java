f0ec93c35831a2f71b55dccd2e07547489e7ebb1
Kay Lee <seoulkay@gmail.com>
2020-08-20 10:56:41 +0900

    Create Application.java

---
+++ ih/Application.java
@@ -0,0 +1,13 @@
+package rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		//어매이징하다!!!!!
+        SpringApplication.run(Application.class, args);
+    }
+}
+
