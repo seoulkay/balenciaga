b31c4f52179babd78871a27bb687219fd401e47d
gracehkpark <gracehkpark@gmail.com>
2020-08-20 09:51:49 +0900

    Create Reply.java

---
+++ hk/Reply.java
@@ -0,0 +1,16 @@
+package pack1;
+
+public class Reply {
+	String id;
+	String content;
+	String date;
+	
+	//생성자
+	public Reply(String id, String content, String date) {
+		this.id = id;
+		this.content = content;
+		this.date = date;
+		
+	}
+
+}
