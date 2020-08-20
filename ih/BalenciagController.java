18477a7c8db92c7d292f2e78ab4045c6ca476abb
Kay Lee <seoulkay@gmail.com>
2020-08-20 10:59:57 +0900

    Create BalenciagController.java

---
+++ ih/BalenciagController.java
@@ -0,0 +1,16 @@
+package rest.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class BalenciagaController {
+	@CrossOrigin(origins = "*")
+	@GetMapping("/sample")
+	public String getSample(){
+		return "성공";
+	}
+}
