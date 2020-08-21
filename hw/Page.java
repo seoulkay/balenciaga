b27006320056be010792c161b217a04c8c8c50ca
mx125 <69613355+mx125@users.noreply.github.com>
2020-08-21 09:46:46 +0900

    Update Page.java

--- hw/Page.java
+++ hw/Page.java
@@ -6,19 +6,23 @@ public class Page {
 	String artistTitle;
 	String songTitle;
 	String releaseDate;
+	String desc;
 	int playCount;
 	int likeCount;
 	
+	//제너릭 
 	ArrayList<Reply> replies = new ArrayList<Reply>();
 	
 	public int getRepliesNumber() {
 		return replies.size();	
 	}
 
-	public Page(String artistTitle, String songTitle, String releaseDate, int playCount, int likeCount) {
+	//생성자 
+	public Page(String artistTitle, String songTitle, String releaseDate, String desc, int playCount, int likeCount) {
 		this.artistTitle = artistTitle;
 		this.songTitle = songTitle;
 		this.releaseDate = releaseDate;
+		this.desc = desc;
 		this.playCount = playCount;
 		this.likeCount = likeCount;
 	}
