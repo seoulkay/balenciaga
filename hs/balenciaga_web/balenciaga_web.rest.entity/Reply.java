816ac6034ab7500e3041a79b77472a987e877be5
tellmebaby <69560522+tellmebaby@users.noreply.github.com>
2020-08-25 18:04:33 +0900

    Create Reply.java

---
+++ hs/balenciaga_web/balenciaga_web.rest.entity/Reply.java
@@ -0,0 +1,87 @@
+package balenciaga_web.rest.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+@Table(name = "Reply")
+public class Reply {
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		private long id;
+
+		@Column(name = "content")
+		private String content;
+
+		@Column(name = "writer")
+		private String writer;
+		
+		@Column(name = "password")
+		private String password;
+		
+		@Column(name = "published")
+		private boolean published;
+
+		public Reply() {
+
+		}
+
+		public Reply(String content, String writer, String password, boolean published) {
+			this.content = content;
+			this.writer = writer;
+			this.password = password;
+			this.published = published;
+		}
+
+		public long getId() {
+			return id;
+		}
+
+		public String getContent() {
+			return content;
+		}
+
+		public void setContent(String content) {
+			this.content = content;
+		}
+
+		public String getWriter() {
+			return writer;
+		}
+
+		public void setWriter(String writer) {
+			this.writer = writer;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		public boolean isPublished() {
+			return published;
+		}
+
+		public void setPublished(boolean isPublished) {
+			this.published = isPublished;
+		}
+
+		@Override
+		public String toString() {
+			return "Tutorial [id=" + id + ", content=" + content + ", writer=" + writer + " , password=" + password + ", published=" + published + "]";
+		}
+
+}
