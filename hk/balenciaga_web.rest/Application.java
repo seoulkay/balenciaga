679940a85a99074de300f03256553f696b0bc9b5
gracehkpark <gracehkpark@gmail.com>
2020-08-25 09:20:03 +0900

    Create Application.java

---
+++ hk/balenciaga_web.rest/Application.java
@@ -0,0 +1,12 @@
+package balenciaga_web.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		//어매이징하다!!!!!
+        SpringApplication.run(Application.class, args);
+    }
+}
