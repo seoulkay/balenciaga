3a93b54261faa5d5694438f242180ea9aadb4b8c
Kay Lee <seoulkay@gmail.com>
2020-08-25 22:45:47 +0900

    Create BalenciagaRepository.java

---
+++ ih/day12/repository/BalenciagaRepository.java
@@ -0,0 +1,16 @@
+package balenciaga_web.rest.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import balenciaga_web.rest.entity.Reply;
+
+public interface BalenciagaRepository extends JpaRepository<Reply, Long> {
+	  List<Reply> findByPublished(boolean published);
+	  List<Reply> findByContentContaining(String content);
+	  Optional<Reply> findById(long id);
+	  Optional<Reply> deleteById(long id);
+	  
+	}
