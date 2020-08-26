d64bb697ecae77dcca19a15483d4bea9127e0b65
tellmebaby <69560522+tellmebaby@users.noreply.github.com>
2020-08-26 20:09:15 +0900

    Update BalenciagaRepsitory.java

--- hs/balenciaga_web/balenciaga_web.rest.repository/BalenciagaRepsitory.java
+++ hs/balenciaga_web/balenciaga_web.rest.repository/BalenciagaRepsitory.java
@@ -1,6 +1,8 @@
 package balenciaga_web.rest.repository;
 
 import java.util.List;
+import java.util.Optional;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +11,6 @@ import balenciaga_web.rest.entity.Reply;
 public interface BalenciagaRepository extends JpaRepository<Reply, Long> {
 	  List<Reply> findByPublished(boolean published);
 	  List<Reply> findByContentContaining(String content);
+	  Optional<Reply> findById(long id);
+	  Optional<Reply> deleteById(long id);
 	}
