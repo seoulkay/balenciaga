e096b1846d9dd825b84c6b15f7809a5190573b59
gracehkpark <gracehkpark@gmail.com>
2020-08-28 13:22:46 +0900

    Update BalenciagaRepository.java

--- hk/balenciaga_web.rest.repository/BalenciagaRepository.java
+++ hk/balenciaga_web.rest.repository/BalenciagaRepository.java
@@ -1,12 +1,19 @@
 package balenciaga_web.rest.repository;
 
-import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import balenciaga_web.rest.entity.Reply;
 
 public interface BalenciagaRepository extends JpaRepository<Reply, Long> {
-	  List<Reply> findByPublished(boolean published);
-	  List<Reply> findByContentContaining(String content);
+	  Page<Reply> findAll(Pageable pageable);
+	  Page<Reply> findByOrderByIdDesc(Pageable pageable);
+	  
+	  Page<Reply> findByPublished(boolean published,  Pageable pageable);
+	  Page<Reply> findByContentContaining(String content,  Pageable pageable);
+	  Optional<Reply> findById(long id);
+	  Optional<Reply> deleteById(long id);
 	}
