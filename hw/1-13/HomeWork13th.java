29052d679f03b2b5d8f1d9dc27754b7477ff6b14
mx125 <69613355+mx125@users.noreply.github.com>
2020-11-05 21:59:35 +0900

    Create HomeWork13th.java

---
+++ hw/1-13/HomeWork13th.java
@@ -0,0 +1,22 @@
+//여기까지 밖에 아직 못함
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class HomeWork13th{
+	public static void main(Integer[] args){
+		ArrayList<Integer> arrayList = new ArrayList<>();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("점수를 입력하세요.");
+
+		while(student>=0){
+			Integer student = sc.nextLine();
+			arrayList.add(student);
+		}
+		for(int i : arrayList){
+			System.out.println("점 : " + i);
+		}
+		System.out.println()
+	}
+}
