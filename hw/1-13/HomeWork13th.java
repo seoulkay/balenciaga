65f5a7beac4ad4c44495470d8f1f80a8f5f9667d
mx125 <69613355+mx125@users.noreply.github.com>
2020-11-06 22:13:01 +0900

    Update HomeWork13th.java

--- hw/1-13/HomeWork13th.java
+++ hw/1-13/HomeWork13th.java
@@ -23,15 +23,37 @@ public class HomeWork13th{
 		System.out.print("학생들의 성적 :  ");
 
 		for(int i = 0; i < arrayList.size(); i++){
-			System.out.print(arrayList.get(i)+"  ");			
+			System.out.print(arrayList.get(i)+"  "); //줄을 띄우지 않고 배열안의 점수를 나열시킨다.			
 		}
-		System.out.println();
+		System.out.println(); // 한 줄 띄운다.
 
-
-
-
+		
+		//최고점의 출발을 0으로 하고 배열에 들어온 수를 차례로 맥스와 비교해서 최고점을 갱신시킨다.
+		
 		int max = 0;
+		
+		for(int i = 0; i < arrayList.size(); i++){
+			if(arrayList.get(i) > max){
+				max = arrayList.get(i);
+			}
+		}
+		
+		//System.out.println(max);  //최고 점수 확인
 
+		
+		//반복
+		//i번 학생의 성적은 get(i)점이며, 등급은 "" 이다. 출력
+		
+		for(int i =0;i<arrayList.size(); i++){
+			if(arrayList.get(i) >= max - 10){
+				System.out.println(i+"번 학생의 성적은" + arrayList.get(i) + "이며, " + "등급은 A이다.");
+			}else if(arrayList.get(i) >= max - 20){
+				System.out.println(i+"번 학생의 성적은" + arrayList.get(i) + "이며, " + "등급은 B이다.");
+			}else{
+				System.out.println(i+"번 학생의 성적은" + arrayList.get(i) + "이며, " + "등급은 C이다.");
+			}
+		}
 
 
 	}
+}
