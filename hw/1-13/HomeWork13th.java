796d830c50bdd0b95290250955845cd1fe644522
mx125 <69613355+mx125@users.noreply.github.com>
2020-11-06 21:58:43 +0900

    Update HomeWork13th.java

--- hw/1-13/HomeWork13th.java
+++ hw/1-13/HomeWork13th.java
@@ -1,24 +1,37 @@
-//아직 미완성.. 이것 밖에 못 했음
-//에러가 많이 뜨고 있음.
-
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class HomeWork13th{
 	public static void main(String[] args){
 		ArrayList<Integer> arrayList = new ArrayList<>();
-
-		System.out.println("점수를 입력하세요.");
-		
 		Scanner sc = new Scanner(System.in);
-		int s = sc.nextInt();
 
-		while(s>=0){
-		arrayList.add(s);
-		}
-
+		while(true){
+			System.out.println("점수를 입력하세요.");
+			int s = sc.nextInt();
+			if(s < 0){
+				break;
+			}
+			arrayList.add(s);
+		}	
+		
 		for(int i : arrayList){
-			System.out.println(i+"점"+\n");
+			System.out.println("점수를 입력하세요 : " + i);
 		}
+
+		System.out.println("전체 학생은 " + arrayList.size()+" 명이다.");		
+		System.out.print("학생들의 성적 :  ");
+
+		for(int i = 0; i < arrayList.size(); i++){
+			System.out.print(arrayList.get(i)+"  ");			
+		}
+		System.out.println();
+
+
+
+
+		int max = 0;
+
+
+
 	}
-}
