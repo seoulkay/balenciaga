2604faff02a94647c1b97d1844684a2e506a07fb
tellmebaby <69560522+tellmebaby@users.noreply.github.com>
2020-11-17 00:32:17 +0900

    Create Link.java

---
+++ hs/1-18/Link.java
@@ -0,0 +1,27 @@
+package www.tellmebaby.shop.entity;
+
+public class Link {
+	private String LinkName;
+	//www.link.com
+	private String linkAddress;
+
+	
+	
+	public String getLinkAddress() {
+		return linkAddress;
+	}
+
+	public void setLinkAddress(String linkAddress) {
+		this.linkAddress = linkAddress;
+	}
+
+	public String getLinkName() {
+		return LinkName;
+	}
+
+	public void setLinkName(String linkName) {
+		LinkName = linkName;
+	}
+	
+
+}
