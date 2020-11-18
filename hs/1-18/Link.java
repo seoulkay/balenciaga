a7b3a97d81ae928394412dc470a2c1f98a5bb6e5
tellmebaby <69560522+tellmebaby@users.noreply.github.com>
2020-11-18 16:48:08 +0900

    Update Link.java

--- hs/1-18/Link.java
+++ hs/1-18/Link.java
@@ -1,27 +1,26 @@
 package www.tellmebaby.shop.entity;
 
 public class Link {
-	private String LinkName;
+	private String linkName;
 	//www.link.com
 	private String linkAddress;
-
 	
 	
+	
+	
+	public String getLinkName() {
+		return linkName;
+	}
+	public void setLinkName(String linkName) {
+		this.linkName = linkName;
+	}
 	public String getLinkAddress() {
 		return linkAddress;
 	}
-
 	public void setLinkAddress(String linkAddress) {
 		this.linkAddress = linkAddress;
 	}
 
-	public String getLinkName() {
-		return LinkName;
-	}
-
-	public void setLinkName(String linkName) {
-		LinkName = linkName;
-	}
 	
 
 }
