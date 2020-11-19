c3ea70693d03b2e635b016489e823d9b3be59bd8
tellmebaby <69560522+tellmebaby@users.noreply.github.com>
2020-11-20 00:31:46 +0900

    Update Sql.sql

--- hs/1-19/Sql.sql
+++ hs/1-19/Sql.sql
@@ -1,1 +1,9 @@
 
+#읽기
+SELECT * FROM hong.Reply;
+#쓰기
+INSERT INTO hong.Reply(instaId, description) VALUES ("inst_22", "드디어 글을 저장할 수 있어...");
+#수정
+UPDATE hong.Reply SET instaId="aaa222", description="이제 수정도 되네요" WHERE id=2;
+#삭제 
+DELETE FROM hong.Reply WHERE id=2;
