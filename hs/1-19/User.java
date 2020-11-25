e1f67d6869084f1859af2760c144d69657b4cb67
tellmebaby <69560522+tellmebaby@users.noreply.github.com>
2020-11-25 23:43:59 +0900

    Create User.java

---
+++ hs/1-19/User.java
@@ -0,0 +1,51 @@
+package www.tellmebaby.shop.entity;
+
+ public class User {
+	 
+	 //사진
+	 //아이디
+	 //실명
+	 
+	 private String picture;
+	 
+	 private String instaId;
+	 
+	 private String name;
+	 
+	 private String passWord;
+
+	
+
+	public String getPassWord() {
+		return passWord;
+	}
+
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+
+	public String getPicture() {
+		return picture;
+	}
+
+	public void setPicture(String picture) {
+		this.picture = picture;
+	}
+
+	public String getInstaId() {
+		return instaId;
+	}
+
+	public void setInstaId(String instaId) {
+		this.instaId = instaId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	 
+ }
